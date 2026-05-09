a09237b762ed39dd2c07376c7a164a17632b862a
artofmethod <sergey.schtein@gmail.com>
2026-05-09 10:49:52 +0300

    Update usercard.js

--- javascripts/discourse-markdown/usercard.js
+++ javascripts/discourse-markdown/usercard.js
@@ -3,42 +3,12 @@ export function setup(helper) {
     return;
   }
 
-  helper.allowList([
-    "span.custom-usercard",
-    "span[data-usercard]",
-    "span[data-username]",
-  ]);
+  helper.allowList(["span.usercard-test"]);
 
   helper.registerPlugin((md) => {
-    md.inline.bbcode.ruler.push("usercard", {
-      tag: "usercard",
-      wrap(startToken, endToken, tagInfo) {
-        const explicitUsername = (tagInfo.attrs._default || "").trim();
-
-        startToken.type = "html_inline";
-
-        if (explicitUsername) {
-          startToken.content = `<span class="custom-usercard" data-usercard="true" data-username="${escapeHtmlAttr(
-            explicitUsername
-          )}">`;
-        } else {
-          startToken.content =
-            `<span class="custom-usercard" data-usercard="true">`;
-        }
-
-        endToken.type = "html_inline";
-        endToken.content = `</span>`;
-
-        return false;
-      },
+    md.inline.bbcode.ruler.push("usercardtest", {
+      tag: "usercardtest",
+      wrap: "span.usercard-test",
     });
   });
-}
-
-function escapeHtmlAttr(value) {
-  return String(value)
-    .replace(/&/g, "&amp;")
-    .replace(/"/g, "&quot;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;");
 }
